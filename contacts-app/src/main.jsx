64ed7b500bd0599a93ae9bf21264d3b533011dd3
Hionel <104563991+Hionel@users.noreply.github.com>
2024-06-10 21:10:18 +0300

    client side rendering added. child component integrated for the root element

--- contacts-app/src/main.jsx
+++ contacts-app/src/main.jsx
@@ -8,11 +8,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
 import ErrorPage from "./routes/error-page";
 
+import Contact from "./routes/contact";
+
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Root />,
 		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: "contacts/:contactId",
+				element: <Contact />,
+			},
+		],
 	},
 ]);
 
