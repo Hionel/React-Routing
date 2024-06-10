2b36dc36295e55573d821d0ec2cc137627740d87
Hionel <104563991+Hionel@users.noreply.github.com>
2024-06-10 21:26:54 +0300

    loader and action added to root route component

--- contacts-app/src/main.jsx
+++ contacts-app/src/main.jsx
@@ -5,9 +5,11 @@ import "./index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Root from "./routes/root";
+import Root, {
+	loader as rootLoader,
+	action as rootAction,
+} from "./routes/root";
 import ErrorPage from "./routes/error-page";
-
 import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
@@ -15,6 +17,8 @@ const router = createBrowserRouter([
 		path: "/",
 		element: <Root />,
 		errorElement: <ErrorPage />,
+		loader: rootLoader,
+		action: rootAction,
 		children: [
 			{
 				path: "contacts/:contactId",
